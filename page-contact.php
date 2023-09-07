9bad590e1ac91f0ebb78c5d39706fa1c78ed09c5
ANONG DIGITAL <143817056+ANONGDIGITAL@users.noreply.github.com>
2023-09-07 16:23:23 +0200

    Add files via upload

---
+++ page-contact.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Entreprise de nettoyage de hottes</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  	<div class="wrap">
+			<div class="container">
+				<div class="row justify-content-between">
+						<div class="col-12 col-md d-flex align-items-center">
+							<p class="mb-0 phone"><span class="mailus">Email :</span> <a href="#">contact@nettoyage-hotte-de-cuisine.fr</a></p>
+						</div>
+						<div class="col-12 col-md d-flex justify-content-md-end">
+							<div class="social-media">
+				    		<p class="mb-0 d-flex">
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+				    		</p>
+			        </div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="myNavbar" id="ftco-navbar">
+	    <div class="container">
+	    	<a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="img" height="35"></a></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	        	<li class="nav-item"><a href="index.html" class="nav-link">Accueil</a></li>
+	        	<li class="nav-item"><a href="hottes-de-cuisine.html" class="nav-link">Hottes de cuisine</a></li>
+	        	<li class="nav-item"><a href="degats-des-eaux.html" class="nav-link">Dégats des eaux</a></li>
+	        	<li class="nav-item"><a href="aspiration-evacuation.html" class="nav-link">Aspiration & évacuation</a></li>
+			  <li class="nav-item"><a href="devis.php" class="nav-link">Devis</a></li>
+	          <li class="nav-item active"><a href="page-contact.php" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/room-with-desk-mirror-floor.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-end">
+          <div class="col-md-9 ftco-animate pb-5">
+          	<p class="breadcrumbs mb-2"><span class="mr-2"><a href="index.html">Accueil <i class="fa fa-chevron-right"></i></a></span> <span>Contact <i class="fa fa-chevron-right"></i></span></p>
+            <h1 class="mb-0 bread">Contact</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+		
+		<section class="ftco-section bg-light">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-md-12">
+						<div class="wrapper">
+							<div class="row mb-5">
+								<div class="col-md-3">
+									<div class="dbox w-100 text-center">
+				        		<div class="icon d-flex align-items-center justify-content-center">
+				        			<span class="fa fa-map-marker"></span>
+				        		</div>
+				        		<div class="text">
+					            <p><span>Adresse :</span> 21 rue de Fécamp, 75012 Paris </p>
+					          </div>
+				          </div>
+								</div>
+								<div class="col-md-3">
+									<div class="dbox w-100 text-center">
+				        		<div class="icon d-flex align-items-center justify-content-center">
+				        			<span class="fa fa-phone"></span>
+				        		</div>
+				        		<div class="text">
+					            <p><span>Téléphone :</span> <a href="tel:0623452926">06 23 45 29 26</a></p>
+					          </div>
+				          </div>
+								</div>
+								<div class="col-md-3">
+									<div class="dbox w-100 text-center">
+				        		<div class="icon d-flex align-items-center justify-content-center">
+				        			<span class="fa fa-paper-plane"></span>
+				        		</div>
+				        		<div class="text">
+					            <p><span>Email:</span> <a href="mailto:">contact.fn75@gmail.com</a></p>
+					          </div>
+				          </div>
+						
+							</div>
+							<div class="row no-gutters">
+								<div class="col-md-7">
+									<div class="contact-wrap w-100 p-md-5 p-4">
+										<h3 class="mb-4">Contactez-nous</h3>
+										<div id="form-message-warning" class="mb-4"></div> 
+
+										<?php include "contact.php"; ?>
+										<form method="POST" id="contactForm" name="contactForm" class="contactForm">
+											<div class="row">
+												<div class="col-md-6">
+													<div class="form-group">
+														<input type="text" class="form-control" name="nom" id="name" placeholder="Nom / Prénom *" require>
+													</div>
+												</div>
+												<div class="col-md-6"> 
+													<div class="form-group">
+														<input type="email" class="form-control" name="email" id="email" placeholder="Email *" require>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="form-group">
+														<input type="text" class="form-control" name="sujet" id="subject" placeholder="Sujet">
+													</div>
+												</div>
+                                                <div class="col-md-6">
+													<div class="form-group">
+														<input type="phone" class="form-control" name="tel" id="phone" placeholder="Téléphone">
+													</div>
+												</div>
+												<div class="col-md-12">
+													<div class="form-group">
+														<textarea name="message" class="form-control" id="message" cols="30" rows="4" placeholder="Message *"></textarea require>
+													</div>
+												</div>
+												<div class="col-md-12">
+													<div class="form-group">
+														<input type="submit" name="envoyer" value="Envoyer le message" class="btn btn-primary">
+														<div class="submitting"></div>
+													</div>
+												</div>
+											</div>
+										</form>
+
+
+
+										<?php if (isset($message)){?>
+											<?php echo' <style>
+											p2 {color: black;
+											}
+											p2 {font-size: 18px;
+											}
+											p2 {font-weight: bold;
+											}
+											p2 {text-align: center;
+											}
+							
+											p2 i {color: #9DD13E;
+											}
+											p2 i {font-size: 25px;
+											}
+											input {color: red; 
+											}
+											
+										
+											</style>
+											<p2><i class="fas fa-check-circle" style="color: #9DD13E;"></i> votre message a bien été envoyé</p2>
+											'; 
+											?> 
+											<?php
+							
+										}elseif(isset($errormsg)){?>
+											<?php echo' <style>
+											p1 {color: red;
+											}
+											p1 {font-size: 15px;
+											}
+											p1 {font-weight: bold;
+											}
+											</style>
+											<p1>vous devez compléter les champs obligatoires</p1>
+											'; 
+											?> 
+											<?php
+										}
+										 ?> 
+
+
+
+
+
+									</div>
+								</div>
+								<div class="col-md-5 d-flex align-items-stretch">
+									<div class="info-wrap w-100 p-5 img" style="background-image: url(images/WhatsApp\ Image\ 2023-07-26\ at\ 20.51.59.jpeg);">
+				          </div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div id="map" class="map"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+    <footer class="footer ftco-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+						<h2 class="footer-heading">Notre entreprise</h2>
+						<p>Flashnet 75 est une entreprise de nettoyage de hottes de cuisine basée à Paris. Nous intervenons principalement pour les professionnels de la restauration mais également chez des partculiers.</p>
+						<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-4">
+							<li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
+							<li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
+							<li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
+						</ul>
+					</div>
+				
+					<div class="col-md-6 col-lg-3 pl-lg-5 mb-4 mb-md-0">
+						<h2 class="footer-heading">Liens utiles</h2>
+						<ul class="list-unstyled">
+							<li><a href="#" class="py-1 d-block">Accueil</a></li>
+							<li><a href="hottes-de-cuisine.html" class="py-1 d-block">Hottes de cuisine</a></li>
+							<li><a href="devis.html" class="py-1 d-block">Devis</a></li>
+							<li><a href="contact.html" class="py-1 d-block">Contact</a></li>
+            			</ul>
+					</div>
+					<div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+						<h2 class="footer-heading">Contact</h2>
+						<div class="block-23 mb-3">
+              <ul>
+                <li><span class="icon fa fa-map-marker"></span><span class="text">21 rue de Fécamp, 75012 Paris</span></li>
+                <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">06 23 45 29 26</span></a></li>
+                <li><a href="#"><span class="icon fa fa-paper-plane"></span><span class="text">contact@nettoyage-hotte-de-cuisine.fr</span></a></li>
+              </ul>
+            </div>
+					</div>
+				</div>
+				<div class="row mt-5">
+          <div class="col-md-12 text-center">
+
+		  <p class="copyright">
+        Copyright &copy; Tous droits réservés. site web créé par <a href="https://anong-digital.fr" target="_blank">Anong Digital</a>
+      </p>
+          </div>
+        </div>
+			</div>
+		</footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+  <script src="script.js"></script>
+    
+  </body>
+</html>
